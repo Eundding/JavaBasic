eddd5d9c9bd2f100609e02e62be4287525aaee14
Eundding <dkan9634@naver.com>
2023-02-28 20:43:31 +0900

    DataTypes

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,17 @@
+package chap_01;
+
+public class _02_DataTypes {
+    // psvm 치면 메인 메소드 자동 생성
+    // main 쳐도 메인 메소드 자동 생성
+    public static void main(String[] args) {
+        // sout 치면 System.out.println(); 자동 생성
+        System.out.println("Hello World"); // 문자열
+        System.out.println(12); // Int
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true); // Bool
+        System.out.println(false);
+        System.out.println(); // 빈 줄
+        System.out.println(123+345); // 계산
+    }
+}
