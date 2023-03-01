dd32200769f37ff62c15ed15118dd1ee18116be7
Eundding <dkan9634@naver.com>
2023-03-01 13:08:22 +0900

    논리연산자

---
+++ src/chap_02/_04_Operator4.java
@@ -0,0 +1,24 @@
+package chap_02;
+
+public class _04_Operator4 {
+    public static void main(String[] args) {
+        // 논리 연산자
+
+        // And 연산 &&
+        System.out.println((5 > 3) && (3 > 1)); // true
+        System.out.println((5 > 3) && (3 < 1)); // false
+
+        // Or 연산 ||
+        System.out.println((5 > 3) || (3 > 1)); // true
+        System.out.println((5 > 3) || (3 < 1)); // true
+
+        // 논리 부정 연삱
+        System.out.println(!true); // false
+        System.out.println(!false); // true
+        System.out.println(!(5==5)); // false
+        System.out.println(!(5==3)); // true
+
+
+
+    }
+}
